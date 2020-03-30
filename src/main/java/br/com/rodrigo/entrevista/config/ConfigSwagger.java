865325103716902d964790cde59564d40b43dbda
Rodrigo Araújo <rodrigo.rsa2005@gmail.com>
2020-03-29 23:53:02 -0300

    ajuste de url

--- src/main/java/br/com/rodrigo/entrevista/config/ConfigSwagger.java
+++ src/main/java/br/com/rodrigo/entrevista/config/ConfigSwagger.java
@@ -3,9 +3,11 @@ package br.com.rodrigo.entrevista.config;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @OpenAPIDefinition(info = @Info(description = "Dólar comercial (venda e compra) ",
                                 title = "API - Cotação do dólar comercial",version = "0.0.1"))
+@ApplicationPath("")
 public class ConfigSwagger extends Application {
 }
